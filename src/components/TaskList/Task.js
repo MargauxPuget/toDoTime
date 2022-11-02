aaa593a4a94311c879ae12cd391731675b4cff93
MargauxPuget <margaux.puget@gmail.com>
2022-11-02 16:43:50 +0100

    On a recuperé les props dans Task et on les utilise pour l'affichage + création d'un affichage dynamique en css

--- src/components/TaskList/Task.js
+++ src/components/TaskList/Task.js
@@ -1,11 +1,48 @@
-function Task({ list }) {
+import PropTypes from 'prop-types';
+
+function Task({
+  label,
+  time,
+  description,
+  done,
+}) {
+  // traduction du time en seconde -> en heures, minutes et secondes.
+  const heures = Math.trunc(time / 60);
+  const minutes = Math.trunc((time - (heures * 3600)) / 60);
+  const secondes = Math.trunc(time % 60);
+  // let checkboxClass = 'list-item ';
+  // if (done) {
+  //   checkboxClass = 'list-item list-item--done';
+  // }
+  // version ternaire
+  // let checkboxClass = done ? ' list-item list-item--done' : ' list-item';
+  // on peut directement placer la ternaire dans le JSX entre accolades
+
   return (
-    <li className="task">
-      <h3>Sortir marcher</h3>
-      <p>en 33 minutes et 15 secondes</p>
-      bar de progression (pê en forme d'horloge{/* TODO :  video a voir 'https://www.youtube.com/watch?v=MdKvSJOWm_U' */})
+    <li className={done ? ' task task--done' : ' task'}>
+      <h3>{label}</h3>
+      <p>en {heures}heures, {minutes}minutes et {secondes} secondes</p>
+      {description}
+      {!done
+      && (
+        <div className="task_buton">
+          <span className="task_buton_text">Fini !</span>
+          <span className="line -right" />
+          <span className="line -top" />
+          <span className="line -left" />
+          <span className="line -bottom" />
+
+        </div>
+      )}
     </li>
   );
 }
+
+Task.propTypes = {
+  label: PropTypes.string.isRequired,
+  time: PropTypes.number.isRequired,
+  description: PropTypes.string.isRequired,
+  done: PropTypes.bool.isRequired,
+};
 
 export default Task;
--- src/data/tasks.js
+++ src/data/tasks.js
@@ -2,29 +2,29 @@ export default [
   {
     id: 1,
     label: 'Sortir marcher',
-    time: 680, // un temps en seconde
-    description: "bar de progression (pê en forme d'horloge :  {/* TODO :  video a voir 'https://www.youtube.com/watch?v=MdKvSJOWm_U' */})",
+    time: 1000, // un temps en seconde
+    description: "bar de progression (pê en forme d'horloge)" /* TODO :  video a voir 'https://www.youtube.com/watch?v=MdKvSJOWm_U' */,
     done: true,
   },
   {
     id: 13,
     label: 'Coder !!!',
-    time: 680, // un temps en seconde
-    description: "bar de progression (pê en forme d'horloge :  {/* TODO :  video a voir 'https://www.youtube.com/watch?v=MdKvSJOWm_U' */})",
+    time: 12318, // un temps en seconde
+    description: "bar de progression (pê en forme d'horloge)",
     done: true,
   },
   {
     id: 4,
     label: 'Payer Nounou',
-    time: 680, // un temps en seconde
-    description: "bar de progression (pê en forme d'horloge :  {/* TODO :  video a voir 'https://www.youtube.com/watch?v=MdKvSJOWm_U' */})",
-    done: true,
+    time: 900, // un temps en seconde
+    description: "bar de progression (pê en forme d'horloge)",
+    done: false,
   },
   {
     id: 8,
     label: 'Prévoir un resto',
-    time: 680, // un temps en seconde
+    time: 600, // un temps en seconde
     description: 'Toutes les 2 semaines',
-    done: true,
+    done: false,
   },
 ];
