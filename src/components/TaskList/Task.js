f092b952039fd75bb1cdc51135b30eaa3481fcf7
MargauxPuget <margaux.puget@gmail.com>
2022-11-02 15:29:08 +0100

    création d'un sous-component Task à TaskList

---
+++ src/components/TaskList/Task.js
@@ -0,0 +1,11 @@
+function Task() {
+  return (
+    <li className="task">
+      <h3>Sortir marcher</h3>
+      <p>en 33 minutes et 15 secondes</p>
+      bar de progression (pê en forme d'horloge{/* TODO :  video a voir 'https://www.youtube.com/watch?v=MdKvSJOWm_U' */})
+    </li>
+  );
+}
+
+export default Task;
--- src/components/TaskList/index.js
+++ src/components/TaskList/index.js
@@ -1,30 +1,14 @@
 // == Import
 import './styles.scss';
+import Task from 'src/components/TaskList/Task';
 
 function TaskList() {
   return (
     <ul className="wrapper_list">
-      <li className="cardItem">
-        <h3>Sortir marcher</h3>
-        <p>en 33 minutes et 15 secondes</p>
-        bar de progression (pê en forme d'horloge :  {/* TODO :  video a voir 'https://www.youtube.com/watch?v=MdKvSJOWm_U' */})
-      </li>
-      <li className="cardItem">
-        <h3>Coder !!!</h3>
-        <p>en 33 minutes et 15 secondes</p>
-        bar de progression (pê en forme d'horloge)
-      </li>
-      <li className="cardItem">
-        <h3>Payer Nounou</h3>
-        <p>en 33 minutes et 15 secondes</p>
-        bar de progression (pê en forme d'horloge)
-      </li>
-      <li className="cardItem">
-        <h3>Prévoir un retso</h3>
-        <p>en 33 minutes et 15 secondes</p>
-        <p>Toutes les 2 semaines</p>
-        bar de progression (pê en forme d'horloge)
-      </li>
+      <Task />
+      <Task />
+      <Task />
+      <Task />
     </ul>
   );
 }
