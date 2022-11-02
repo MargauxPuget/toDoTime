b8b5ca301f3d9630bd5dc4858b8e29c42d5cd975
MargauxPuget <margaux.puget@gmail.com>
2022-11-02 15:08:47 +0100

    création d'un component Counter

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,12 +1,15 @@
 // == Import
 import './styles.css';
 import Form from 'src/components/Form';
+import Counter from 'src/components/Counter';
 
 // == Composant
 function App() {
   return (
     <div className="app">
       <Form />
+      <Counter />
+
     </div>
   );
 }
---
+++ src/components/Counter/index.js
@@ -0,0 +1,13 @@
+// == Import
+import './styles.scss';
+
+function Counter() {
+  return (
+    <div className="wrapper_couter">
+      <p className="counter">X tâches en attentes</p>
+      <p className="counter">Y tâches finies</p>
+    </div>/* wrapper_couter */
+  );
+}
+
+export default Counter;
