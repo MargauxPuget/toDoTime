316d8749c5b424c6094e76f6e07ca646f2ecd522
MargauxPuget <margaux.puget@gmail.com>
2022-11-03 09:50:39 +0100

    ajout d'un listener onClick sur le bouton du formulaire

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -2,12 +2,18 @@
 import './styles.scss';
 
 function Form() {
+  function handleSubmit(event) {
+    event.preventDefault();
+    console.log('Je veux ajouter une tache !!');
+  }
+
   return (
     <div className="wrapper_form">
       <h2> Ajouter une tâche et un temps</h2>
-      <form className="form">
+      <form className="form" onSubmit={handleSubmit}>
         <input type="text" className="form_item" placeholder="Ajouter une tâche" />
         <input type="time" className="form_item" />
+        <button type="button" onClick={handleSubmit} className="form_button">Création</button>
       </form>
     </div>
   );
